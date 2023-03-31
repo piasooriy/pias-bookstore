6e41fb7fec32476469f64f5df0a5fea32704b1ae
camillamn <112381378+camillamn@users.noreply.github.com>
2023-03-31 08:05:51 +0200

    Feature: Add searchmodule (#41)
    * Added searchmodule
    * Refactored the code

---
+++ _app/js/modules/book-search.js
@@ -0,0 +1,97 @@
+import {sanity} from '../sanity.js';
+
+export default async function SearchList() {
+	let bookSearchText = '';
+	let bookResults = [];
+
+	const bookListInput = document.querySelector('.book-list__input');
+	const bookListResults = document.querySelector('.book-list__results');
+	const bookListSearchButton = document.querySelector('.book-list__search-button');
+
+	bookListSearchButton.addEventListener('click', handleSearchButtonClick);
+	bookListInput.addEventListener('keyup', handleSearchInputKeyup);
+
+
+	function handleSearchButtonClick() {
+		setBookSearchText(bookListInput);
+		fetchBooks();
+		renderHTML();
+	}
+
+	function handleSearchInputKeyup(event) {
+		if (event.key === 'Enter') {
+			setBookSearchText(event.target);
+			fetchBooks();
+			renderHTML();
+		}
+	}
+
+	function setBookSearchText(input) {
+		bookSearchText = input.value;
+	}
+
+	async function fetchBooks() {
+		const query = `*[_type == 'book' && title match $bookTitle + '*'] {
+			title,
+			"bookCover": cover.asset->url,
+			"author": author->lastName,
+			price,
+		}`;
+
+		const params = {
+			bookTitle: bookSearchText
+		}
+
+		bookResults = await sanity.fetch(query, params);
+		console.log(bookResults);
+	}
+
+	function createBookListContainerDOM() {
+		console.log(bookResults);
+		const bookListContainer = document.createElement('div');
+		
+		bookListContainer.className = 'book-list__container';
+
+		for (const book of bookResults) {
+			const bookCard = document.createElement('div');
+			const bookTitle = document.createElement('figcaption');
+			const bookCover = document.createElement('img');
+			const authorName = document.createElement('div');
+			const bookPrice = document.createElement('div');
+			const bookCoverBox = document.createElement('figure')
+
+
+			bookListContainer.appendChild(bookCoverBox);
+			bookListContainer.appendChild(bookCard);
+			bookCoverBox.appendChild(bookCover);
+			bookCoverBox.appendChild(bookTitle);
+			bookCard.appendChild(authorName);
+			bookCard.appendChild(bookPrice);		
+			
+			bookCard.className= 'book__card';
+			bookCoverBox.className= 'book__cover-box';
+
+
+			bookCover.className= 'book-cover';
+			bookCover.src = book.bookCover;
+
+			bookTitle.className= 'book-title';
+			bookTitle.innerText = book.title;
+
+			authorName.className= 'author__name';
+			authorName.innerText = book.author;
+
+			bookPrice.className= 'book__price';
+			bookPrice.innerText = book.price;
+		}
+
+		return bookListContainer;
+	}
+
+	function renderHTML() {
+		const bookListContainer = createBookListContainerDOM();
+		bookListResults.innerHTML = '';
+		bookListResults.appendChild(bookListContainer);
+	}
+
+}
