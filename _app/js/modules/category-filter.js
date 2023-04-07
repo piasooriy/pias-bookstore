63d83abf0ac2f1b37423b467697a94872f523033
Pia sooriy <112381491+piasooriy@users.noreply.github.com>
2023-04-07 16:09:41 +0200

    Made a renderHMTL and Booklistcontainer function

--- _app/js/modules/category-filter.js
+++ _app/js/modules/category-filter.js
@@ -1,36 +1,113 @@
 import { sanity } from "../sanity.js";
 
 export default async function CategoryFilter(){
-	const query = `
-		*[_type == 'book']{  
-			title,
-			"category": category->name, 
+  let books = [];
+
+  const categorySelect = document.querySelector('.category__filter-select');
+  const bookListResults = document.querySelector('.book-list__results');
+  
+  
+  // Fetch categories
+  const categoriesQuery = `*[_type == 'category']{
+	 name 
 	}`;
-		
+  const categories = await sanity.fetch(categoriesQuery);
 
-	const categories = await sanity.fetch(query);
+  // Make options
+  const allCategoriesOption = document.createElement('option');
+  allCategoriesOption.value = 'all';
+  allCategoriesOption.innerText = 'All';
+  categorySelect.appendChild(allCategoriesOption);
+  
+  /*https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/values  */
+  for (const category of categories) {
+    const categoryOption = document.createElement('option');
+    categoryOption.value = category.name;
+    categoryOption.innerText = category.name;
+    categorySelect.appendChild(categoryOption);
+  }
 
-	function createCategoryElementDOM(){
-		return bookCategoryContainer;
+  // Fetch all books 
+  
+  const booksQuery = `*[_type == "book"]{  
+    title,
+    cover,
+    author->{firstName, lastName},
+    price,
+    "categories": categories[]->name
+  }`;
+  books = await sanity.fetch(booksQuery);
+ 
+  // Eventlistener to choose categories
+  categorySelect.addEventListener('change', () => {
+    const selectedCategory = categorySelect.value;
+    const filteredBooks = filterBooksByCategory(books, selectedCategory);
+    const filteredBookListContainer = createBookListContainerDOM(filteredBooks);
+    renderHTML(filteredBookListContainer);
+  });
+
+  /* https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/includes
+   https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter*/
+
+  /* making filterBooks */ 
+  function filterBooksByCategory(books, selectedCategory) {
+	if (selectedCategory === 'all') {
+	  return books;
+	} else {
+	return books.filter(book => book.categories && book.categories.includes(selectedCategory))}
+ 	};
+
+  /* making filteredBookListContainer */
+  function createBookListContainerDOM(books) {
+   
+    const bookListContainer = document.createElement('div');
+
+	  bookListContainer.className='book-list';
+
+    for (const book of books) {
+      const bookCard = document.createElement('div');
+      const bookCoverBox = document.createElement('figure')
+      const bookTitle = document.createElement('figcaption');
+      const bookCover = document.createElement('img');
+      const authorName = document.createElement('div');
+      const bookPrice = document.createElement('div');
+
+      bookListContainer.appendChild(bookCoverBox);
+			bookListContainer.appendChild(bookCard);
+			bookCoverBox.appendChild(bookCover);
+			bookCoverBox.appendChild(bookTitle);
+			bookCard.appendChild(authorName);
+			bookCard.appendChild(bookPrice);
+
+      
+      bookCard.className= 'book__card';
+      bookCoverBox.className= 'book__cover-box';
+
+      bookCover.className= 'book-cover';
+      bookCover.src = book.cover.asset.url;
+      bookCover.alt = book.cover.alt;
+
+      bookTitle.className= 'book-title';
+      bookTitle.innerText = book.title;
+
+      authorName.className= 'author__name';
+      authorName.innerText = `${book.author.firstName} ${book.author.lastName}`;
+
+      bookPrice.className= 'book__price';
+      bookPrice.innerText = book.price;
 	}
+	
 
-	function renderHTML(){
-		const bookCategoryContainer = createCategoryElementDOM();
-		document.body.appendChild(bookCategoryContainer);
-	}	
+    return bookListContainer;
 
-	renderHTML();
+	}
+	
 
-	let currentFilter = null;
-	let filteredCategories = returnFilteredCategories();
+  function renderHTML(filteredBookListContainer) {
+	bookListResults.innerHTML = '';
+	bookListResults.appendChild(filteredBookListContainer);
+  
+}
 
-	const bookCategoryContainer = document.createElement('select');	
-	bookCategoryContainer.className = 'book__category-container';
-
-	const bookCategory = document.createElement('option');
-	bookCategory.className = 'book__category';
-	bookCategory.innerHTML = book.category;
-
-	bookCategoryContainer.appendChild(bookCategory);
-
+  renderHTML();
 }
