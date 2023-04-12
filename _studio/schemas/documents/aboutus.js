5ae81929a07b1cf10d2332701fed25d20e203999
Pia sooriy <112381491+piasooriy@users.noreply.github.com>
2023-04-12 11:45:55 +0200

    Added about us module under schema

---
+++ _studio/schemas/documents/aboutus.js
@@ -0,0 +1,13 @@
+export default {
+	title: 'About Us', 
+	name: 'aboutUs', 
+	type: 'document', 
+	fields: [
+		{
+			title: 'History', 
+			name: 'history', 
+			type: 'string'
+		}, 
+		
+	]
+}
--- _studio/schemas/schemas.js
+++ _studio/schemas/schemas.js
@@ -1,12 +1,14 @@
-import book from "./documents/book";
-import author from "./documents/author";
-import category from "./documents/category";
-import tag from "./documents/tag";
+import book from './documents/book';
+import author from './documents/author';
+import category from './documents/category';
+import tag from './documents/tag';
+import aboutus from './documents/aboutus';
 
 export default [
 	book,
 	author,
 	category,
 	tag,
+	aboutus,
 
 ]
